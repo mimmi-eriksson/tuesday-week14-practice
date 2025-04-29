510c3971724ca5ba3823de2015155a55886f68ee
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-04-29 11:35:31 +0200

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,13 @@ export const App = () => {
   const [count, setCount] = useState(0)
   return (
     <>
-      <h1>React Boilerplate</h1>
-
-      <button onClick={() => setCount(count + 1)}>
-        Increase count</button>
-      <button onClick={() => setCount(count - 1)}>
-        Decrease count</button>
-      <button
-        onClick={() => setCount(0)}
-        disabled={count === 0}
-      >
-        Reset
-      </button>
-      <button onClick={() => setCount(count * 2)}>
-        Multiply</button>
-
+      <h1>React State</h1>
       <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
+      <button onClick={() => setCount(count * 2)}>Multiply</button>
       {count > 100 && <p>You hit 100!</p>}
-
     </>
   )
 }
